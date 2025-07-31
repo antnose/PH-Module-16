af78ad3cc5ac49e77ffdb9d1028a989a14949fea
antnose <antnose28@gmail.com>
2025-07-31 20:50:33 +0600

    Introduction to Objects properties and values

---
+++ JS-String-Object/object.js
@@ -0,0 +1,25 @@
+const age = 21;
+const school = "Rifle Square";
+const isPassed = true;
+// let isDeveloper;
+const sub = ["Bengali", "English", "Physics", "Math"];
+
+// Object
+const bottle = {
+  brand: "Apple",
+  price: 24,
+  isCold: true,
+  color: "white",
+};
+
+const subject = {
+  name: "Biology",
+  teacher: "Rasheda Mam",
+  examDate: "30 feb",
+  chapter: ["First", "Second", "Third"],
+  exams: {
+    name: "Final Exam",
+    marks: 100,
+  },
+};
+console.log(subject);
