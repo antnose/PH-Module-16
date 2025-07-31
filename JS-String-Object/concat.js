a09501e6e78ab39fcbc9d58f5d1c74f133332f75
antnose <antnose28@gmail.com>
2025-07-31 20:33:48 +0600

    learn about Slice, Join, Concat and includes

---
+++ JS-String-Object/concat.js
@@ -0,0 +1,6 @@
+const first = "Abid";
+const second = "Nabid";
+console.log(`${first} ${second}`);
+
+// includes
+console.log(second.includes("id"));
---
+++ JS-String-Object/slice.js
@@ -0,0 +1,10 @@
+const address = "AndorKilla";
+const part = address.slice(0, 5);
+// console.log(part);
+
+const sentence = "I am not hardworking person";
+// console.log(sentence.split(" "));
+// console.log(sentence.split("a"));
+const friends = "Rohim, Korim, Adbul, Ruhul, Sabbir";
+const anotherFriend = friends.split(",");
+// console.log(anotherFriend.join("-"));
