90a1acf9a33811d478ab4818d1486a7823fa1a4d
antnose <antnose28@gmail.com>
2025-07-31 20:59:41 +0600

    Multiple ways to get & set object properties

---
+++ JS-String-Object/property.js
@@ -0,0 +1,13 @@
+const person = {
+  name: "Sodor Uddin",
+  age: 24,
+  profession: "Developer",
+  salary: 254000,
+  married: true,
+};
+// console.log(person.name);
+// console.log(person["profession"]);
+
+person.salary = 5006000060;
+person["age"] = 26;
+console.log(person);
---
+++ JS-String-Object/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Sodor Uddin",
+  age: 24,
+  profession: "Developer",
+  salary: 254000,
+  married: true,
+};
+// console.log(person.name);
+// console.log(person["profession"]);
+
+person.salary = 5006000060;
+console.log(person);
