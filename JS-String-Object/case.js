fd042f0b9c715db53c4cc69900cfa34496843c5f
antnose <antnose28@gmail.com>
2025-07-31 20:24:14 +0600

    know about case and trim method

---
+++ JS-String-Object/case.js
@@ -0,0 +1,16 @@
+const school = "Rajuk Uttara Model School";
+// console.log(school);
+// console.log(school.toLowerCase());
+
+const subject = "Chemistry";
+const book = "chemistry";
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+  console.log(`I am reading book ${book}`);
+} else {
+  console.log(`Hudai Hudai Prishta Ultai`);
+}
+
+const drink = "water";
+const liquid = "      water";
+console.log(liquid.trim());
